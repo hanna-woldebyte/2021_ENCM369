f88b1738dcc56c3ad5c4a801e3acd598f76ba70d
hanna-woldebyte <hanna.woldegebriel@calgary.ca>
2021-03-31 20:27:41 -0600

    created a new folder

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=pic5.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/pic5.X.production.hex
+CND_ARTIFACT_NAME_default=picactivity_6.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/picactivity_6.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=pic5.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/pic5.x.tar
+CND_PACKAGE_NAME_default=picactivity6.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/picactivity6.x.tar
